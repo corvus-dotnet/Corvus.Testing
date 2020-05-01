a1145608f980aafe4afb9abdde6ff62c794d716b
Alastair Smith <a.smith@iesohealth.com>
2020-05-01 08:52:37 +0100

    Add tests for configured function per fixture
    Define a new class fixture `ConfiguredAzureFunctionFixture` to set up
    the required configuration before running the tests. There's not a nicer
    way in Xunit AFAICT to use a class fixture for a shared function and set
    up the configuration outside of that fixture, but this is probably
    forcing me down a better path wrt Single Responsibility, etc.
    
    Again, the tests methods themselves are duplicated copypasta-style. It
    might be worth at this point deduplicating the three copies into one.

---
+++ Solutions/Corvus.Testing.AzureFunctions.Xunit.Demo/ConfiguredAzureFunctionFixture.cs
@@ -0,0 +1,37 @@
+// <copyright file="ConfiguredAzureFunctionFixture.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Testing.AzureFunctions.Xunit.Demo
+{
+    using System;
+    using System.Threading.Tasks;
+    using global::Xunit;
+
+    public class ConfiguredAzureFunctionFixture : IAsyncLifetime
+    {
+        private readonly FunctionsController function = new FunctionsController();
+
+        public int Port => 7077;
+
+        public string Greet(string name) => $"Welcome, {name}";
+
+        public async Task InitializeAsync()
+        {
+            var configuration = new FunctionConfiguration();
+            configuration.EnvironmentVariables.Add("ResponseMessage", this.Greet("{name}"));
+
+            await this.function.StartFunctionsInstance(
+                Environment.CurrentDirectory,
+                "Corvus.SpecFlow.Extensions.DemoFunction",
+                this.Port,
+                configuration: configuration);
+        }
+
+        public async Task DisposeAsync()
+        {
+            this.function.TeardownFunctions();
+            await Task.CompletedTask;
+        }
+    }
+}
