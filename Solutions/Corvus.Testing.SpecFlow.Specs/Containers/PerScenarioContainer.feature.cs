e4fed87c29761595bb8960eb48b6db2ee58023a9
Jonathan George <Jonathan.George@endjin.com>
2020-05-06 17:29:24 +0100

    Second round of renames

--- Solutions/Corvus.Testing.SpecFlow.Specs/Containers/PerScenarioContainer.feature.cs
+++ Solutions/Corvus.Testing.SpecFlow.Specs/Containers/PerScenarioContainer.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Corvus.SpecFlow.Extensions.Specs.Containers
+namespace Corvus.Testing.SpecFlow.Specs.Containers
 {
     using TechTalk.SpecFlow;
     using System;
